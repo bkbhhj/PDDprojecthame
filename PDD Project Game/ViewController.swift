c6a9a6257d36ed4f01ca0669ef23494031dab7d0
Pavel <ttt.1.12@bk.ru>
2021-04-12 23:24:06 +0300

    Initial Commit

---
+++ PDD Project Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDD Project Game
+//
+//  Created by Павел Тимофеев on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
