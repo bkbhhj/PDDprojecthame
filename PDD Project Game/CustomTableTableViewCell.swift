3123734fb8caf302dd27f270efb098a1a86bf24a
Pavel <ttt.1.12@bk.ru>
2021-04-16 20:14:01 +0300

    Initial commit

---
+++ PDD Project Game/CustomTableTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableTableViewCell.swift
+//  PDD Project Game
+//
+//  Created by Павел Тимофеев on 14.04.2021.
+//
+
+import UIKit
+
+class CustomTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PDD Project Game/DetailVC.swift
@@ -0,0 +1,8 @@
+//
+//  DetailVC.swift
+//  PDD Project Game
+//
+//  Created by Павел Тимофеев on 15.04.2021.
+//
+
+import Foundation
