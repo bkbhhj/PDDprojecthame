56f8bca39cad739a22bdbc8651e989430bc172bb
Pavel <ttt.1.12@bk.ru>
2021-04-23 19:42:20 +0300

    i added the main logic in my project. I have just to add Image for every called cell.

--- PDD Project Game/DetailVC.swift
+++ PDD Project Game/DetailVC.swift
@@ -11,7 +11,7 @@ class DetailVC: UIViewController {
 
     @IBOutlet var nameRoadSignLabel: UILabel!
     var arraySign = [String]()
-    
+    var correctAnswer = 0
     @IBOutlet var buttomOne: UIButton!
     @IBOutlet var buttonTwo: UIButton!
     @IBOutlet var buttonThree: UIButton!
@@ -29,12 +29,55 @@ class DetailVC: UIViewController {
         sixth()
         seventh()
         eighth()
-        guard let a = arraySign.randomElement() else {return}
-        nameRoadSignLabel.text = """
-            Выберите знак
-            \(a)
-           """
+askQuestion()
     }
+    func askQuestion (action: UIAlertAction! = nil) {
+        arraySign.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        buttomOne.setImage(UIImage(named: arraySign[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: arraySign[1]), for: .normal)
+        buttonThree.setImage(UIImage(named: arraySign[2]), for: .normal)
     
+        nameRoadSignLabel.text = """
+                        Выберите знак
+                       \(arraySign[correctAnswer])
+                       """
+    }
 
+    
+    @IBAction func actionButton(_ sender: UIButton) {
+      var  title:  String!
+        if sender.tag == correctAnswer {
+            title = "Здорово"
+        } else {
+            title = "Плохо"
+        }
+        func alerts () {
+            let alertWrongController = UIAlertController(title: title, message: " ТЫ выбрал неправильный знак! Знак который ты выбрал называется - \(arraySign[sender.tag])! ", preferredStyle: .alert)
+            alertWrongController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(alertWrongController, animated: true)
+        }
+        switch sender.tag {
+         case 0 :
+        if sender.tag != correctAnswer {
+         alerts() }
+         case 1 :
+         if sender.tag != correctAnswer {
+         alerts() }
+         case 2 :
+         if sender.tag != correctAnswer {
+         alerts()
+            }
+        default:
+            break
+        }
+
+        
+   let ac = UIAlertController(title: title, message: "Красава. Десяточка.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        }
+    
+    
+    
 }
